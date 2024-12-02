7075de65573dc9cd5ddc6341d8f8741dfbb41ef6
Mahmutov Azat <cudraviymalay@gmail.com>
2024-12-02 16:28:38 +0300

    вроде сделал 3 из 5 пунктов

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,51 @@
+package hiber.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "cars")
+public class Car implements Serializable {
+
+    @Id
+    @OneToOne()
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "series")
+    private int series;
+
+    public Car() {
+
+    }
+
+    public Car(User user, int series, String model) {
+        this.user = user;
+        this.series = series;
+        this.model = model;
+    }
+
+    public User getUser() { return user; }
+
+    public void setUser(User user) { this.user = user; }
+
+    public String getModel() { return model; }
+
+    public void setModel(String model) { this.model = model; }
+
+    public int getSeries() { return series; }
+
+    public void setSeries(int series) { this.series = series; }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "user=" + user +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+}
